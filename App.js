ad294eb88aeae6472ad1aa637a1806f2067d0f4e
ludeathefer <ahblagarpnew@gmail.com>
2023-12-25 20:43:54 +0545

    Initializing Project

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Styles from "./src/StyleHolder";
+
+export default function App() {
+  return (
+    <View style={Styles.container_Default}>
+      <Text>Let's HUSTLE</Text>
+    </View>
+  );
+}
---
+++ src/StyleHolder.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import {
+  horizontalScale,
+  verticalScale,
+  moderateScale,
+} from "../assets/Constants/measurements";
+const Styles = StyleSheet.create({
+  container_Default: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Styles;
