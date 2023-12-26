c62b6d9e8377b25b17bb70117e159b6532ff8919
ludeathefer <ahblagarpnew@gmail.com>
2023-12-26 06:12:11 +0545

    NormalUserBookings Fixed

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 import Styles from "./src/StyleHolder";
-import MyBookings_Consumer from "./src/screens/MyBookings_Consumer";
 import { StatusBar } from "react-native";
 import MyBookings_Provider from "./src/screens/MyBookings_Provider";
 const statusBarHeight = StatusBar.currentHeight || 0;
+import NormalBooking from "./src/screens/NormalUser/NormalBooking";
 import ViewMaps from "./src/screens/Maps";
 
 export default function App() {
@@ -11,8 +11,8 @@ export default function App() {
     <View style={[Styles.container_Default, { marginTop: statusBarHeight }]}>
       <StatusBar barStyle="light-content" backgroundColor="red" translucent />
       {/* <MyBookings_Provider /> */}
-      {/* <MyBookings_Consumer /> */}
-      <ViewMaps />
+      <NormalBooking />
+      {/* <ViewMaps /> */}
     </View>
   );
 }
